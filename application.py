897359654793258609f8fc17c570e00c7b5622fb
volante <adrien.volante@yahoo.com>
2021-04-20 20:06:01 +0200

    modif appli

--- application.py
+++ application.py
@@ -9,6 +9,21 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 
+# Importing our best model for prediction
+from sklearn.ensemble import GradientBoostingClassifier
+
+GB = GradientBoostingClassifier(max_depth = 8, max_features = 'sqrt',
+                           min_samples_leaf = 50, min_samples_split = 1000,
+                           random_state = 10, subsample = 0.8)
+
+# Importing our data_stats
+stat =  pd.read_csv('C:/Users/VOLANTE/anaconda3/envs/OC/P7/X_stats.csv', index_col = 0)
+
+# Importing our X_train and y_train
+y_train = pd.read_csv('C:/Users/VOLANTE/anaconda3/envs/OC/P7/y_train.csv', index_col = 0)
+X_train = pd.read_csv('C:/Users/VOLANTE/anaconda3/envs/OC/P7/X_train.csv', index_col =0)
+
+############################################################################################################################
 
 st.title('Prêt à dépenser BANK')
 
@@ -20,9 +35,9 @@ st.write('''
 
 # Title on sidebar
 st.sidebar.header('Your informations')
+st.sidebar.write('Please respect the order!')
 
-# Importing our data_stats
-stat =  pd.read_csv('C:/Users/VOLANTE/anaconda3/envs/OC/P7/X_stats.csv', index_col = 0)
+
 
 def user_input():
     '''Creating a function which generates a DataFrame, accordind to consumer's inputs '''
@@ -35,7 +50,7 @@ def user_input():
     DAYS_ID_PUBLISH = st.sidebar.number_input('Last Identity document change (in days, between {} and {})'.format(int(stat['DAYS_ID_PUBLISH']['min']), int(stat['DAYS_ID_PUBLISH']['max'])), min_value = int(stat['DAYS_ID_PUBLISH']['min']), max_value = int(stat['DAYS_ID_PUBLISH']['max']))
     AMT_GOODS_PRICE = st.sidebar.number_input('Good price Amount (Between {}$ and {}$ allowed)'.format(stat['AMT_GOODS_PRICE']['min'], stat['AMT_GOODS_PRICE']['max']), min_value = stat['AMT_GOODS_PRICE']['min'], max_value = stat['AMT_GOODS_PRICE']['max'])
     AMT_CREDIT = st.sidebar.number_input('Credit Amount', min_value = stat['AMT_CREDIT']['min'], max_value = (stat['CREDIT_GOOD_PERCENT']['max']) * AMT_GOODS_PRICE)
-    AMT_ANNUITY = st.sidebar.number_input('Annuity Amount', min_value = (stat['CREDIT_ANNUITY_PERCENT']['min'])*AMT_CREDIT, max_value = (stat['CREDIT_ANNUITY_PERCENT']['max'])*AMT_CREDIT)
+    AMT_ANNUITY = st.sidebar.number_input('Annuity Amount', min_value = (stat['CREDIT_ANNUITY_PERCENT']['min'])*AMT_CREDIT, max_value = (stat['ANNUITY_INCOME_PERCENT']['max'])*AMT_INCOME_TOTAL)
     EXT_SOURCE_2 = st.sidebar.slider('Extern source 2', stat['EXT_SOURCE_2']['min'], stat['EXT_SOURCE_2']['max'], 0.53)
     EXT_SOURCE_3 = st.sidebar.slider('Extern source 3', stat['EXT_SOURCE_3']['min'], stat['EXT_SOURCE_3']['max'], 0.51)
     
@@ -65,21 +80,21 @@ def user_input():
 # df is the output of our function
 df = user_input()
 
-st.subheader('Your Data')
+st.subheader('Your Data. Please check before validate!')
 st.write(df)
 
 ##################################################################################################################
-# Creating our 4 other features for model
-df['CREDIT_INCOME_PERCENT'] = df['AMT_CREDIT'] / df['AMT_INCOME_TOTAL']
-df['ANNUITY_INCOME_PERCENT'] = df['AMT_ANNUITY'] / df['AMT_INCOME_TOTAL']
-df['CREDIT_ANNUITY_PERCENT'] = df['AMT_ANNUITY'] / df['AMT_CREDIT']
-df['CREDIT_GOOD_PERCENT'] = df['AMT_CREDIT'] / df['AMT_GOODS_PRICE']  
+def adding_columns(df):
+
+    # Creating our 4 other features for model
+    df['CREDIT_INCOME_PERCENT'] = df['AMT_CREDIT'] / df['AMT_INCOME_TOTAL']
+    df['ANNUITY_INCOME_PERCENT'] = df['AMT_ANNUITY'] / df['AMT_INCOME_TOTAL']
+    df['CREDIT_ANNUITY_PERCENT'] = df['AMT_ANNUITY'] / df['AMT_CREDIT']
+    df['CREDIT_GOOD_PERCENT'] = df['AMT_CREDIT'] / df['AMT_GOODS_PRICE']  
 
 
 ##################################################################################################################
 #Encoding AGE
-
-
 def encode(x):
     if 20.0 <x<= 25.0:
         return 0
@@ -108,73 +123,64 @@ df['AGE'] = df['AGE'].apply(encode)
 
 ###################################################################################################################
 
-# Importing our X_train and y_train
-y_train = pd.read_csv('C:/Users/VOLANTE/anaconda3/envs/OC/P7/y_train.csv', index_col = 0)
-X_train = pd.read_csv('C:/Users/VOLANTE/anaconda3/envs/OC/P7/X_train.csv', index_col =0)
 
 
 # MinMaxScaler formula
 # X_scaled = (X - X.min(axis=0)) / (X.max(axis=0) - X.min(axis=0))
 
+def scaling(df):
 
+    df.loc[0, 'CNT_CHILDREN'] = (df.loc[0, 'CNT_CHILDREN'] - stat['CNT_CHILDREN']['min']) / (stat['CNT_CHILDREN']['max'] - stat['CNT_CHILDREN']['min'] )
 
-df.loc[0, 'CNT_CHILDREN'] = (df.loc[0, 'CNT_CHILDREN'] - stat['CNT_CHILDREN']['min']) / (stat['CNT_CHILDREN']['max'] - stat['CNT_CHILDREN']['min'] )
+    df.loc[0, 'AMT_INCOME_TOTAL'] = (df.loc[0, 'AMT_INCOME_TOTAL'] - stat['AMT_INCOME_TOTAL']['min']) / (stat['AMT_INCOME_TOTAL']['max'] - stat['AMT_INCOME_TOTAL']['min']) 
 
-df.loc[0, 'AMT_INCOME_TOTAL'] = (df.loc[0, 'AMT_INCOME_TOTAL'] - stat['AMT_INCOME_TOTAL']['min']) / (stat['AMT_INCOME_TOTAL']['max'] - stat['AMT_INCOME_TOTAL']['min']) 
+    df.loc[0, 'DAYS_EMPLOYED'] = (df.loc[0, 'DAYS_EMPLOYED'] - stat['DAYS_EMPLOYED']['min']) / (stat['DAYS_EMPLOYED']['max'] - stat['DAYS_EMPLOYED']['min'])
 
-df.loc[0, 'DAYS_EMPLOYED'] = (df.loc[0, 'DAYS_EMPLOYED'] - stat['DAYS_EMPLOYED']['min']) / (stat['DAYS_EMPLOYED']['max'] - stat['DAYS_EMPLOYED']['min'])
+    df.loc[0, 'DAYS_REGISTRATION'] = (df.loc[0, 'DAYS_REGISTRATION'] - stat['DAYS_REGISTRATION']['min']) / (stat['DAYS_REGISTRATION']['max'] - stat['DAYS_REGISTRATION']['min'])
 
-df.loc[0, 'DAYS_REGISTRATION'] = (df.loc[0, 'DAYS_REGISTRATION'] - stat['DAYS_REGISTRATION']['min']) / (stat['DAYS_REGISTRATION']['max'] - stat['DAYS_REGISTRATION']['min'])
+    df.loc[0, 'DAYS_ID_PUBLISH'] = (df.loc[0, 'DAYS_ID_PUBLISH'] - stat['DAYS_ID_PUBLISH']['min']) / (stat['DAYS_ID_PUBLISH']['max'] - stat['DAYS_ID_PUBLISH']['min'])
 
-df.loc[0, 'DAYS_ID_PUBLISH'] = (df.loc[0, 'DAYS_ID_PUBLISH'] - stat['DAYS_ID_PUBLISH']['min']) / (stat['DAYS_ID_PUBLISH']['max'] - stat['DAYS_ID_PUBLISH']['min'])
+    df.loc[0, 'AMT_GOODS_PRICE'] = (df.loc[0, 'AMT_GOODS_PRICE'] - stat['AMT_GOODS_PRICE']['min']) / (stat['AMT_GOODS_PRICE']['max'] - stat['AMT_GOODS_PRICE']['min'])
 
-df.loc[0, 'AMT_GOODS_PRICE'] = (df.loc[0, 'AMT_GOODS_PRICE'] - stat['AMT_GOODS_PRICE']['min']) / (stat['AMT_GOODS_PRICE']['max'] - stat['AMT_GOODS_PRICE']['min'])
+    df.loc[0, 'AMT_CREDIT'] = (df.loc[0, 'AMT_CREDIT'] - stat['AMT_CREDIT']['min']) / (stat['AMT_CREDIT']['max'] - stat['AMT_CREDIT']['min'] )
 
-df.loc[0, 'AMT_CREDIT'] = (df.loc[0, 'AMT_CREDIT'] - stat['AMT_CREDIT']['min']) / (stat['AMT_CREDIT']['max'] - stat['AMT_CREDIT']['min'] )
+    df.loc[0, 'AMT_ANNUITY'] = (df.loc[0, 'AMT_ANNUITY'] - stat['AMT_ANNUITY']['min']) / (stat['AMT_ANNUITY']['max'] - stat['AMT_ANNUITY']['min'] )
 
-df.loc[0, 'AMT_ANNUITY'] = (df.loc[0, 'AMT_ANNUITY'] - stat['AMT_ANNUITY']['min']) / (stat['AMT_ANNUITY']['max'] - stat['AMT_ANNUITY']['min'] )
+    df.loc[0, 'EXT_SOURCE_2'] = (df.loc[0, 'EXT_SOURCE_2'] - stat['EXT_SOURCE_2']['min']) / (stat['EXT_SOURCE_2']['max'] - stat['EXT_SOURCE_2']['min'])
 
-df.loc[0, 'EXT_SOURCE_2'] = (df.loc[0, 'EXT_SOURCE_2'] - stat['EXT_SOURCE_2']['min']) / (stat['EXT_SOURCE_2']['max'] - stat['EXT_SOURCE_2']['min'])
+    df.loc[0, 'EXT_SOURCE_3'] = (df.loc[0, 'EXT_SOURCE_3'] - stat['EXT_SOURCE_3']['min']) / (stat['EXT_SOURCE_3']['max'] - stat['EXT_SOURCE_3']['min'])
 
-df.loc[0, 'EXT_SOURCE_3'] = (df.loc[0, 'EXT_SOURCE_3'] - stat['EXT_SOURCE_3']['min']) / (stat['EXT_SOURCE_3']['max'] - stat['EXT_SOURCE_3']['min'])
+    df.loc[0, 'CREDIT_INCOME_PERCENT'] = (df.loc[0, 'CREDIT_INCOME_PERCENT'] - stat['CREDIT_INCOME_PERCENT']['min']) / (stat['CREDIT_INCOME_PERCENT']['max'] - stat['CREDIT_INCOME_PERCENT']['min'] )
 
-df.loc[0, 'CREDIT_INCOME_PERCENT'] = (df.loc[0, 'CREDIT_INCOME_PERCENT'] - stat['CREDIT_INCOME_PERCENT']['min']) / (stat['CREDIT_INCOME_PERCENT']['max'] - stat['CREDIT_INCOME_PERCENT']['min'] )
+    df.loc[0, 'ANNUITY_INCOME_PERCENT'] = (df.loc[0, 'ANNUITY_INCOME_PERCENT'] - stat['ANNUITY_INCOME_PERCENT']['min']) / (stat['ANNUITY_INCOME_PERCENT']['max'] - stat['ANNUITY_INCOME_PERCENT']['min'])
 
-df.loc[0, 'ANNUITY_INCOME_PERCENT'] = (df.loc[0, 'ANNUITY_INCOME_PERCENT'] - stat['ANNUITY_INCOME_PERCENT']['min']) / (stat['ANNUITY_INCOME_PERCENT']['max'] - stat['ANNUITY_INCOME_PERCENT']['min'])
+    df.loc[0, 'CREDIT_ANNUITY_PERCENT'] = (df.loc[0, 'CREDIT_ANNUITY_PERCENT'] - stat['CREDIT_ANNUITY_PERCENT']['min']) / (stat['CREDIT_ANNUITY_PERCENT']['max'] - stat['CREDIT_ANNUITY_PERCENT']['min'])
 
-df.loc[0, 'CREDIT_ANNUITY_PERCENT'] = (df.loc[0, 'CREDIT_ANNUITY_PERCENT'] - stat['CREDIT_ANNUITY_PERCENT']['min']) / (stat['CREDIT_ANNUITY_PERCENT']['max'] - stat['CREDIT_ANNUITY_PERCENT']['min'])
+    df.loc[0, 'CREDIT_GOOD_PERCENT'] = (df.loc[0, 'CREDIT_GOOD_PERCENT'] - stat['CREDIT_GOOD_PERCENT']['min']) / (stat['CREDIT_GOOD_PERCENT']['max'] - stat['CREDIT_GOOD_PERCENT']['min'] )
 
-df.loc[0, 'CREDIT_GOOD_PERCENT'] = (df.loc[0, 'CREDIT_GOOD_PERCENT'] - stat['CREDIT_GOOD_PERCENT']['min']) / (stat['CREDIT_GOOD_PERCENT']['max'] - stat['CREDIT_GOOD_PERCENT']['min'] )
 
 ####################################################################################################################
 
-# Importing our best model for prediction
-from sklearn.ensemble import GradientBoostingClassifier
 
-GB = GradientBoostingClassifier(max_depth = 8, max_features = 'sqrt',
-                           min_samples_leaf = 50, min_samples_split = 1000,
-                           random_state = 10, subsample = 0.8)
+def preprocessing (X_train):
+    # Preprocessing
+    from sklearn.preprocessing import MinMaxScaler
 
-####################################################################################################################
+    # Giving 0 for min and 1 for max.. for the rest the proportion
+    scaler = MinMaxScaler()
 
-# Preprocessing
-from sklearn.preprocessing import MinMaxScaler
-
-# Giving 0 for min and 1 for max.. for the rest the proportion
-scaler = MinMaxScaler()
-
-#Scaling X_train excepted  'age'
-for col in X_train.drop('AGE', axis = 1).columns :
-    X_train.loc[:, col] = scaler.fit_transform(X_train[col].values.reshape(-1, 1))
+    #Scaling X_train excepted  'age'
+    for col in X_train.drop('AGE', axis = 1).columns :
+        X_train.loc[:, col] = scaler.fit_transform(X_train[col].values.reshape(-1, 1))
     
-# Encoding 'AGE'
-from sklearn.preprocessing import LabelEncoder
+    # Encoding 'AGE'
+    from sklearn.preprocessing import LabelEncoder
+    
+    # Creating OneHotEncoder
+    encoder = LabelEncoder()
 
-# Creating OneHotEncoder
-encoder = LabelEncoder()
-
-# Encoding 'AGE' for X_train
-X_train['AGE'] = encoder.fit_transform(X_train['AGE'].values.reshape(-1, 1))
+    # Encoding 'AGE' for X_train
+    X_train['AGE'] = encoder.fit_transform(X_train['AGE'].values.reshape(-1, 1))
 
 
 
@@ -195,35 +201,36 @@ if st.button('How does it work?'):
     st.write('In other words, your input informations must respect some statistical values (according to our bank policy)')
 
 
-
-# Converting in arrays numpy
-
-df = np.array(df)
-X_train =  np.array(X_train)
-y_train =  np.array(y_train)
-
-
-
-# Fitting model
-GB.fit(X_train, y_train)
-
-# If button is clicked, then we apply our algorithm for input datas and give result
-if st.button('See results'):
+if st.sidebar.button('Validate/See results'):
     
+    adding_columns(df)
+    scaling(df)
+    preprocessing(X_train)
     
-   
-    
+    # Converting in arrays numpy
+    df = np.array(df)
+    X_train =  np.array(X_train)
+    y_train =  np.array(y_train)
+
+    # Fitting model
+    GB.fit(X_train, y_train)
+
     
     #import pdb;pdb.set_trace()
     # Prediction
     prediction = GB.predict(df)
+    prob = GB.predict_proba(df)
+    
+   
     
     if prediction == 0:
-        st.write('ACCEPTED')
-    else :
-        st.write('REFUSED')
-    
-
+        st.write('ACCEPTED', prob)
+        st.write("Under 0 you have the probability you won't be in defaut payment")
+        st.write("Under 1 you have the probability you will be in defaut payment")
+    else:
+        st.write('REFUSED', prob)
+        st.write("Under 0 you have the probability that you won't be in defaut payment")
+        st.write("Under 1 you have the probability you will be in defaut payment")
 
 
 
