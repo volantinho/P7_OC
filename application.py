6134c3112501d8c4fa1c2a26f5804b06b45a1ebf
volante <adrien.volante@yahoo.com>
2021-04-29 13:50:51 +0200

    correction

--- application.py
+++ application.py
@@ -14,7 +14,7 @@ import pickle
 # Files importations####################################################################################################
 
 # 12 statistics table (12 clusters)
-stat0 = pd.read_csv ('C:/Users/VOLANTE/Desktop/projet_7/stat1.csv', index_col = 0)
+stat0 = pd.read_csv ('C:/Users/VOLANTE/Desktop/projet_7/stat0.csv', index_col = 0)
 stat1 = pd.read_csv ('C:/Users/VOLANTE/Desktop/projet_7/stat1.csv', index_col = 0)
 stat2 = pd.read_csv ('C:/Users/VOLANTE/Desktop/projet_7/stat2.csv', index_col = 0)
 stat3 = pd.read_csv ('C:/Users/VOLANTE/Desktop/projet_7/stat3.csv', index_col = 0)
